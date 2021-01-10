111da3948e18c1d77249468ae4552b7116f4c669
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2021-01-10 13:11:14 +0100

    Experiment with headers

---
+++ yellow_belt/material/include.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+#include "include.h"
+#include "include.h"
+
+using namespace std;
+
+int main() {
+    PrintLine("Hello World!");
+    return 0;
+}
---
+++ yellow_belt/material/include.h
@@ -0,0 +1,8 @@
+#pragma once
+
+using namespace std;
+
+void PrintLine(const string& str) {
+
+    cout << str << endl;
+}
