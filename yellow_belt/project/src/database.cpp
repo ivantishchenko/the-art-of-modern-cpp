522bf4fa5078cb1f98aa450ae9eeb7278242f4d7
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2021-01-14 19:26:15 +0100

    Started implementing a db

--- yellow_belt/project/src/database.cpp
+++ yellow_belt/project/src/database.cpp
@@ -1,1 +1,38 @@
 #include "database.h"
+
+#include <utility>
+
+
+void Database::Add(const Date& date, const string& event) {
+
+}
+
+void Database::Print(ostream& out) const {
+
+}
+void Database::Print() const {
+
+}
+
+template <typename Predicate> 
+int Database::RemoveIf(const Predicate& pred) {
+
+}
+
+template <typename Predicate>
+set<string> Database::FindIf(const Predicate& pred) const {
+
+}
+
+pair<Date, string> Database::Last(const Date& date) const {
+
+}
+
+map<Date, set<string>> Database::GetEvents() const {
+
+}
+
+ostream& operator<<(ostream& out, const Database& db) {
+    db.Print(out);
+    return out;
+}
