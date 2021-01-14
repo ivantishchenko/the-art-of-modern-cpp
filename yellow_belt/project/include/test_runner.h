ecedf54ae59a3abf1060620564676db65e4212d0
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2021-01-14 19:07:13 +0100

    Refactored testing framework

--- yellow_belt/project/include/test_runner.h
+++ yellow_belt/project/include/test_runner.h
@@ -11,41 +11,15 @@
 using namespace std;
 
 template<class T, class U>
-void AssertEqual(const T& t, const U& u,
-    const string& hint)
-{
-  if (t != u) {
-    ostringstream os;
-    os << "Assertion failed: " << t << " != " << u
-       << " hint: " << hint;
-    throw runtime_error(os.str());
-  }
-}
+void AssertEqual(const T& t, const U& u, const string& hint);
 
-inline void Assert(bool b, const string& hint) {
-  AssertEqual(b, true, hint);
-}
+void Assert(bool b, const string& hint);
 
 class TestRunner {
 public:
   template <class TestFunc>
-  void RunTest(TestFunc func, const string& test_name) {
-    try {
-	  func();
-	  cerr << test_name << " OK" << endl;
-	} catch (runtime_error& e) {
-	  ++fail_count;
-	  cerr << test_name << " fail: " << e.what() << endl;
-	}
-  }
-
-  ~TestRunner() {
-    if (fail_count > 0) {
-      cerr << fail_count << " unit tests failed. Terminate" << endl;
-      exit(1);
-    }
-  }
-
+  void RunTest(TestFunc func, const string& test_name);
+  ~TestRunner();
 private:
   int fail_count = 0;
 };
--- yellow_belt/project/src/test_runner.cpp
+++ yellow_belt/project/src/test_runner.cpp
@@ -1,5 +1,37 @@
 #include "test_runner.h"
 
+template<class T, class U>
+void AssertEqual(const T& t, const U& u, const string& hint) {
+  if (t != u) {
+    ostringstream os;
+    os << "Assertion failed: " << t << " != " << u
+       << " hint: " << hint;
+    throw runtime_error(os.str());
+  }
+}
+
+inline void Assert(bool b, const string& hint) {
+  AssertEqual(b, true, hint);
+}
+
+template <class TestFunc>
+void TestRunner::RunTest(TestFunc func, const string& test_name) {
+    try {
+        func();
+        cerr << test_name << " OK" << endl;
+    } catch (runtime_error& e) {
+        ++fail_count;
+        cerr << test_name << " fail: " << e.what() << endl;
+    }
+}
+
+TestRunner::~TestRunner() {
+    if (fail_count > 0) {
+        cerr << fail_count << " unit tests failed. Terminate" << endl;
+        exit(1);
+    }
+}
+
 template <typename Collection>
 string Join(const Collection& c, char d) {
   stringstream ss;
