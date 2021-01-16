b62e07fd5c75efecdfdcca0324ac582c4e8cf025
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2021-01-16 15:29:45 +0100

    Black magic with the testing framework

--- yellow_belt/project/include/database.h
+++ yellow_belt/project/include/database.h
@@ -13,31 +13,10 @@ public:
     void Add(const Date& date, const string& event);
     void Print(ostream& out) const;
     void Print() const;
-    template <typename Predicate>
-    size_t RemoveIf(const Predicate& pred) {
-        size_t removed_count = 0; 
-        for(const auto& [date, events] : storage_) {
-            for(const auto& event: events) {
-                if(pred(date, event)) {
-                    storage_[date].erase(event);
-                    removed_count++;
-                }
-            }
-        }
-        return removed_count;
-    }
-    template <typename Predicate>
-    inline map<Date, string> FindIf(const Predicate& pred) const {
-        map<Date, string> res;
-        for(const auto& [date, events] : storage_) {
-            for(const auto& event: events) {
-                if(pred(date, event)) {
-                    res.insert(make_pair(date, event));
-                }
-            }
-        }
-        return res;
-    }
+    template <typename Predicate> 
+    size_t RemoveIf(const Predicate& pred);
+    template <typename Predicate> 
+    map<Date, string> FindIf(const Predicate& pred) const;
     pair<Date, string> Last(const Date& date) const;
     map<Date, set<string>> GetStorage() const;
 private:
@@ -45,3 +24,30 @@ private:
 };
 
 ostream& operator<<(ostream& out, const Database& db);
+
+template <typename Predicate>
+size_t Database::RemoveIf(const Predicate& pred) {
+    size_t removed_count = 0; 
+    for(const auto& [date, events] : storage_) {
+        for(const auto& event: events) {
+            if(pred(date, event)) {
+                storage_[date].erase(event);
+                removed_count++;
+            }
+        }
+    }
+    return removed_count;
+}
+
+template <typename Predicate>
+map<Date, string> Database::FindIf(const Predicate& pred) const {
+    map<Date, string> res;
+    for(const auto& [date, events] : storage_) {
+        for(const auto& event: events) {
+            if(pred(date, event)) {
+                res.insert(make_pair(date, event));
+            }
+        }
+    }
+    return res;
+}
