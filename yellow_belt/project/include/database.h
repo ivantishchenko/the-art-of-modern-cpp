f0856300d715c0ade334942ba7d5153561a51af7
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2021-01-17 12:19:24 +0100

    Implemented Last with stack

--- yellow_belt/project/include/database.h
+++ yellow_belt/project/include/database.h
@@ -7,6 +7,7 @@
 #include <map>
 #include <iostream>
 #include <vector>
+#include <stack>
 
 class Database {
 public:
@@ -20,7 +21,8 @@ public:
     pair<Date, string> Last(const Date& date) const;
     map<Date, set<string>> GetStorage() const;
 private:
-    map<Date, set<string>> storage_;
+    map<Date, set<string>> date_to_sorted;
+    map<Date, stack<string>> date_to_stack;
 };
 
 ostream& operator<<(ostream& out, const Database& db);
@@ -28,10 +30,10 @@ ostream& operator<<(ostream& out, const Database& db);
 template <typename Predicate>
 size_t Database::RemoveIf(const Predicate& pred) {
     size_t removed_count = 0; 
-    for(const auto& [date, events] : storage_) {
-        for(const auto& event: events) {
+    for(const auto& [date, events] : date_to_sorted) {
+        for(const string& event: events) {
             if(pred(date, event)) {
-                storage_[date].erase(event);
+                date_to_sorted.at(date).erase(event);
                 removed_count++;
             }
         }
@@ -42,7 +44,7 @@ size_t Database::RemoveIf(const Predicate& pred) {
 template <typename Predicate>
 map<Date, string> Database::FindIf(const Predicate& pred) const {
     map<Date, string> res;
-    for(const auto& [date, events] : storage_) {
+    for(const auto& [date, events] : date_to_sorted) {
         for(const auto& event: events) {
             if(pred(date, event)) {
                 res.insert(make_pair(date, event));
--- yellow_belt/project/src/database.cpp
+++ yellow_belt/project/src/database.cpp
@@ -5,11 +5,12 @@
 #include <iterator>
 
 void Database::Add(const Date& date, const string& event) {
-    storage_[date].insert(event);
+    date_to_sorted[date].insert(event);
+    date_to_stack[date].push(event);
 }
 
 void Database::Print(ostream& out) const {
-    for(const auto& [date, event_set]: storage_) {
+    for(const auto& [date, event_set]: date_to_sorted) {
         for(const auto& event: event_set) {
             out << date << ' ' << event << endl;
         }
@@ -20,17 +21,20 @@ void Database::Print() const {
 }
 
 pair<Date, string> Database::Last(const Date& date) const {
-    auto upper_bound_it = storage_.upper_bound(date);
-    if(upper_bound_it == begin(storage_)) {
+    auto upper_bound_it = date_to_sorted.upper_bound(date);
+    if(upper_bound_it == begin(date_to_sorted)) {
         throw invalid_argument("No entries");
     }
     upper_bound_it = prev(upper_bound_it);
-    // TODO: Improve the event with timestamp or a queue
-    return make_pair(upper_bound_it->first, *begin(upper_bound_it->second));
+    
+    Date last_date = upper_bound_it->first;
+    string last_event = date_to_stack.at(last_date).top();
+
+    return make_pair(last_date, last_event);
 }
 
 map<Date, set<string>> Database::GetStorage() const {
-    return storage_;
+    return date_to_sorted;
 }
 
 ostream& operator<<(ostream& out, const Database& db) {
